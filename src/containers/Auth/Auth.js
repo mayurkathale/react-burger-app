16f88a9f63532125fba6d4e0e06cc44323d8d413
Mayur Kathale <mayurkathale@Mayurs-MacBook-Pro.local>
2021-02-08 21:04:27 +0700

    adding new reducer for auth reducer

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
 
 class Auth extends Component {
     state = {
@@ -33,7 +36,49 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
+        },
+        isSignUp: true
+    }
+
+    checkValidity (value, rules) {
+        let isValid = true;
+
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.minLength && isValid;
+        }
+        return isValid;
+    }
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        }
+        this.setState({controls: updatedControls})
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value,this.state.controls.password.value, this.state.isSignUp);
+    } 
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {isSignUp: !prevState.isSignUp};
+        });
     }
 
     render() {
@@ -56,17 +101,26 @@ class Auth extends Component {
                 invalid={!formElement.config.valid}
                 shouldValidate={formElement.config.validation}
                 touched={formElement.config.touched}
-                changed={(event) => this.changedHandler(event, formElement.id)} />
+                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ));
         return (
-            <div>
-                <form>
-                {form}
-                <Button btnType="Success">Submit</Button>
+            <div className={classes.Auth}>
+                <form onSubmit={this.submitHandler}>
+                    {form}
+                    <Button btnType="Success">Submit</Button>
                 </form>
+                <Button 
+                    btnType="Danger"
+                    clicked={this.switchAuthModeHandler} >Switch to {this.state.isSignUp ? 'Sign up' : 'Sign in' }</Button>
             </div>
         )
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+    }
+} 
+
+export default connect(null, mapDispatchToProps)(Auth);
---
+++ src/store/actions/auth.js
@@ -0,0 +1,49 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+}
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+}
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+}
+
+export const auth = (email, password, isSignUp) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBK_WyLHOvQd0WR8u911ELmCpXqvL10dQc';
+        if(!isSignUp) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBK_WyLHOvQd0WR8u911ELmCpXqvL10dQc';
+        }
+        axios.post(
+            url,
+            authData
+        ).then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
+        })
+        .catch(err => {
+            dispatch(authFail(err));
+        })
+    }
+}
+export default authStart;
